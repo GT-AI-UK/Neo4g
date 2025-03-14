f0e7aa3ad0f8c2a15e0c41e3b94f2ca4d820dd8e
GT-AI-UK <georgethomasnov1993@gmail.com>
2025-03-14 21:46:08 +0000

    lots of testing to do with the query builder now. Refactoring the macro would be good to do as well

--- neo4g/src/query_builder.rs
+++ neo4g/src/query_builder.rs
@@ -5,19 +5,6 @@ use neo4rs::{BoltNull, BoltType, Graph, Node, Relation, Query};
 use std::marker::PhantomData;
 use std::fmt;
 
-// Could start Neo4gBuilder again but use statements as functions too?
-// eg.
-// Neo4gBuilder::new().create().node(entity).relation(enitty2).node(entity3).ret(e1, e2, e3).run().await?
-// Neo4gBuilder::new().get().node(entity).relation(en2).node(en3).where(props).ret(e1, e3).run().await? //get instead of match
-// Neo4gBuilder::new().merge(None).node(e1).zero_plus().relation(e2).node(e3).on_match_set(props).on_create_set(props).with(e1, e3).merge_ref(e1).relation(e4).node(e5).run().await?
-// // unsure whether to have merge take params... can I create a hashmap in the query builder for which nodes are which aliases? Can I validate aliases or are they better as &str?
-// instead of using merge with optional tuple, could have a .props() method? .merge().node(node).props(props)?
-// should additional_labels be a special type of field, or just exlude it from macros and assume it exists? C_ould probably create the field in the macro as a pub Vec<String>
-// .where may need to be named differently? similar to match...
-// using ref of previous node/rel would be interesting too, methods with _ref appended? conditions to call these would be that the statement hasn't just started, so would need yet more states for builder to be in?
-// If doing this, need to have differernt structs to navigate between for each different clause?
-// Structs/Traits to be in the form: <Clause><PreviousAction>, eg. MergeReferencedNode, MergeReferencedRelation, MatchRefNode, MatchRefRelation
-
 use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
@@ -29,7 +16,7 @@ pub struct Neo4gBuilder<State> {
     return_refs: Vec<(String, EntityType, EntityWrapper)>,
     order_by_str: String,
     previous_entity: Option<(String, EntityType, EntityWrapper)>,
-    clause: Clause, // use clause to determine what .node and .relation call. permissions for where will be interesting. 
+    clause: Clause,
     _state: PhantomData<State>,
 }
 
